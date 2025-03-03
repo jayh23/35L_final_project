0544dcd58dc84a3250d03534c6ef14fb2a064daa
pegifenu <110061199+pegifenu@users.noreply.github.com>
2025-03-02 18:24:58 -0800

    Protected Login and Signup routes after user logs in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext'
 
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
@@ -6,14 +7,15 @@ import Login from './pages/Login'
 import Signup from './pages/Signup'
 
 function App() {
+    const { user } = useAuthContext()
 
     return (
         <>
             <Navbar />
             <Routes>
                 <Route path="/home" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+                <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />} />
             </Routes>
 
         </>
